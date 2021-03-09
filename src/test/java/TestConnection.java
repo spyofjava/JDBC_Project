b669e8f93e40bf7f3a8aed62e8e4800d119064e0
spyofjava <73192225+spyofjava@users.noreply.github.com>
2021-03-09 12:17:09 -0800

    Initial commit

---
+++ src/test/java/TestConnection.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class TestConnection {
+    /**
+     *   host : 54.236.150.168
+     *   port : 1521
+     *   SID  : XE
+     *   user : hr
+     *   pass : hr
+     *
+     *   jdbc url AKA connection String
+     *   syntax :
+     *   jdbc : vendorName : driverType @YourHost:PORT:SID
+     *
+     *   jdbc:oracle:thin:@54.236.150.168:1521:XE
+     *
+     */
+    public static void main(String[] args){
+
+
+        String url = "jdbc:oracle:thin:@54.236.150.168:1521:XE" ;
+        try {
+            Connection con = DriverManager.getConnection(url, "hr", "hr");
+
+            System.out.println("CONNECTION SUCCESSFUL");
+
+        }catch(SQLException e){
+            System.out.println("CONNECTION HAS FAILED "+ e.getMessage());
+        }
+
+    }
+
+}
---
+++ src/test/java/day1/JDBC_FirstStep.java
@@ -0,0 +1,50 @@
+package day1;
+
+import java.sql.*;
+
+public class JDBC_FirstStep {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:oracle:thin:@54.236.150.168:1521:XE" ;
+        try {
+            Connection con = DriverManager.getConnection(url, "hr", "hr");
+            // This way creating statement object
+            // will allow us to move forward and backward easily when navigating through the result we got
+            Statement stmnt  = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY) ;
+
+            ResultSet rs = stmnt.executeQuery("SELECT * FROM REGIONS")  ;
+
+            // IN ORDER TO ACCESS RESULTSET , WE NEED TO KNOW WHICH ROW WE ARE AT
+            // BEFORE ENTERING THE FIRST ROW , THE CURSOR OF THE RESULTSET IS AT THE LOCAITON KNOWN AS --BEFORE FIRST
+            // IN ORDER TO MOVE TO NEXT LINE  WE NEED TO CALL next method of resultset
+            // IR ORDER TO GET ANY COLUMN DATA WE NEED TO CALL method getString or getDouble .... and so on
+            rs.next() ;
+            // now we are at the first row
+            System.out.println("FIRST ROW REGION_NAME IS "  +  rs.getString("REGION_NAME") ) ;
+
+            rs.next() ;
+            System.out.println("SECOND ROW REGION_NAME IS "  +  rs.getString("REGION_NAME") ) ;
+
+            rs.next() ;
+            System.out.println("THIRD ROW REGION_NAME IS "  +  rs.getString("REGION_NAME") ) ;
+
+            rs.next() ;
+            System.out.println("FORTH ROW REGION_NAME IS "  +  rs.getString("REGION_NAME") ) ;
+
+            rs.next() ; // THIS WILL MOVE THE CURSOR TO THE LOCATION KNOW AS -- AFTER LAST
+            // BELOW LINE WILL NOT WORK!!!! THERE AINT NO ROW
+            //  System.out.println("FIFTH ROW REGION_NAME IS "  +  rs.getString("REGION_NAME") ) ;
+
+
+
+            System.out.println("CONNECTION SUCCESSFUL");
+
+        }catch(SQLException e){
+            System.out.println("ERROR OCCURRED "+ e.getMessage());
+        }
+
+
+    }
+
+}
